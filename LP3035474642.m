901ca54b701df94b09fb70377070981cdd7612dc
fantinee <mvrojas4@uc.cl>
2020-04-16 23:06:17 -0400

    Phase II case 1 added

--- LP3035474642.m
+++ LP3035474642.m
@@ -1,4 +1,4 @@
-function [data, info] = LP3035474642(A, b,c)
+function [data, info] = LP3035474642(A, b, c)
 %
 % setup LP
 %
@@ -9,84 +9,42 @@ flg = 0; %% initialize flg.
 
 %
 % STARTING SIMPLEX METHOD:
-%% STARTING PHASE I SIMPLEX METHOD
+% STARTING PHASE I SIMPLEX METHOD
 % Computes basic feasible solution
 
 A_phaseI = [A eye(m)]; %% A extended with I(m)
 e_phaseI = ones(1,m); %% e = (1 .. 1)
-c_phaseI = [zeros(1,n) e_phaseI]; %% new obj for phase I 
+c_phaseI = [zeros(1,n) e_phaseI]'; %% new obj for phase I 
 x_phaseI = [zeros(n,1); b]; %% x = [0 ... 0 b]'
 B_phaseI = find(x_phaseI~=0); %% index from phase first Basis
 
-%% STARTING PHASE II SIMPLEX METHOD
+% getting feasible solution 
 
-obj = c'*x;
-disp(['Initial Objective = ', num2str(obj)]);
-%disp('Displaying Initial solution x, c-A^T*y and their componentwise product');
-%disp([x c-A'*y x.*(c-A'*y)]);
-simplex = 1;
-ITER = 0;
-%pause(2);
-while (simplex == 1)
-%
-% determine the next s and r values.
-%
-   y        = T(end,2:end)';
-   [zmin,s] = min(c-A'*y); 
-%
-% check for convergence.
-%
-   if (abs(zmin) < 1e-14)
-       disp('Simplex Method has converged');
-       simplex = 0;
-%       disp('Displaying Optimal Basis');
-%       disp(B');
-       x   = zeros(n,1);
-       x(B) = T(1:end-1,1);
-       obj  = c'*x;
-       disp(['Optimal Objective = ', num2str(obj),' after ', num2str(ITER), ' iterations']);
-       disp('Displaying Optimal solution x, c-A^T*y and their componentwise product');
-       disp([x c-A'*y x.*(c-A'*y)]);
-       continue;
-   end
+[flg, x0, y0, B0, t0, s0, r0, obj0] = Phase2(A_phaseI,b,c_phaseI,x_phaseI,B_phaseI); %% Solution phase I
 
-   t        = T(1:end-1,2:end)*A(:,s);
-   [flg,r] = Revisedgetr(n,s,B,T,t);
-   if (flg == 1)
-       info.run = 'Failure'; % Failure case
-       info.msg = 'Failure due degeneracy'; 
-       %disp('LP is degenerate');
-       simplex = 0;
-       continue;
-   end
-   if (r < 1)
-       disp('LP has no lower bound');
-       simplex = 0;
-       continue;
-   end
-   x   = zeros(n,1);
-   x(B)= T(1:end-1,1);
-   ITER = ITER + 1;
-   f = ['Iteration ', num2str(ITER), ' Obj ', num2str(c'*x), '. Smallest component in c-A^T*y: ', ... 
-         num2str(zmin), ' at s =', num2str(s), '. Component r = ', num2str(r), ' out of basis'];
-%   disp(f);
-   obj1 = c'*x;
-%
-% update the revised simplex tableau.
-%
-   [T,B1,flg]=RevisedSimplexTableau(B,r,s,t,zmin,T);      
-   if (flg == 1)
-       %disp('LP is degenerate');
-       info.run = 'Failure'; % Failure case
-       info.msg = 'Failure due degeneracy'; 
-       simplex = 0;
-       continue;
-   end
-   B   = B1;
-   obj = obj1;
-%   disp('Current Basis is');
-%   disp(B');
-%   pause(1);
+if (flg==1)
+  info.run = "Failure";
+  info.msg = "Unable to compute a feasible solution in Phase I";
 end
-clear B1 f obj1 t zmin
+
+data.PhaseI.obj = obj0;
+data.PhaseI.x = x0;
+
+if (obj0 > 0) %% case where is not possible to form a feasible solution with x variables
+  info.run = "Failure";
+  info.msg = "Sum over Z!=0, not able to get a feasible solution."; 
+end
+
+%if (obj0 == 0) we move on to Phase II
+% STARTING PHASE II SIMPLEX METHOD
+
+x_phaseII = x0(1:n);
+
+info.case = 1;
+[flg, x, y, B, t, s, r, obj] = Phase2(A,b,c,x_phaseII,B0); %% Solution phase II
+data.PhaseII.PrimalObj = obj;
+data.PhaseII.x = x;
+data.PhaseII.y = y;
+data.PhaseII.z =c - (A')*y;
+
 end
